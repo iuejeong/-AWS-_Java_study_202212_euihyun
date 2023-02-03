daefa417b2c95664f57c8ba86b499db44c97f631
iuejeong <dmlgus3585@naver.com>
2023-02-03 17:51:24 +0900

    Python lotto, 보행자도로평균폭, 평균값 계산

--- src/j04_연산자/Operation4.java
+++ src/j04_연산자/Operation4.java
@@ -3,6 +3,7 @@ package j04_연산자;
 public class Operation4 {
 
 	public static void main(String[] args) {
+		
 		int num = 100;
 		
 		num = num + 1;
--- src/j05_입력/input1.java
+++ src/j05_입력/input1.java
@@ -3,28 +3,21 @@ package j05_입력;
 import java.util.Scanner;
 
 public class input1 {
-
 	public static void main(String[] args) {
-		
 		Scanner scanner = new Scanner(System.in);
-		int num1 = 0;
-		int num2 = 0;
-		int num3 = 0;
-		int num4 = 0;
-		int num5 = 0;
-		
-		System.out.print("입력1: ");
-		 num1 = scanner.nextInt();
-		System.out.print("입력2: ");
-		 num2 = scanner.nextInt();
-		System.out.print("입력3: ");
-		 num3 = scanner.nextInt();
-		System.out.print("입력4: ");
-		 num4 = scanner.nextInt();
-		System.out.print("입력5: ");
-		 num5 = scanner.nextInt();
-		
-		
-		System.out.println("입력값: " + (num1 + num2 + num3 + num4 + num5));
+		int[] nums = new int[5];
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			System.out.print("입력" + (i + 1) + ": ");
+			if (scanner.hasNextInt()) {
+				nums[i] = scanner.nextInt();
+				sum += nums[i];
+			} else {
+				System.out.println("Invalid input, please enter an integer.");
+				scanner.next();
+				i--;
+			}
+		}
+		System.out.println("입력값: " + sum);
 	}
 }
